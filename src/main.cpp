5e5216db972809e44894e84cf608f9e5a36856cb
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2017-07-18 16:47:19 -0700

    Clean up main and get table from Prompter

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,9 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/features2d/features2d.hpp>
-
 #include <boost/filesystem.hpp>
-
 #include <iostream>
 #include "Prompter.h"
 
-using namespace cv;
 using namespace std;
 using namespace boost::filesystem;
-
-void findCircle() {
-    string name("data/a-s.jpg");
-    Mat image;
-    image = imread(name.c_str());
-
-    if (image.empty()) {
-        cout << "Could not open image file" << endl;
-        return;
-    }
-
-
-    Mat gray;
-    cvtColor(image, gray, COLOR_BGR2GRAY);
-
-
-    GaussianBlur(gray, gray, Size(9, 9), 2, 2);
-
-    vector<Vec3f> circles;
-
-    HoughCircles(gray, circles, HOUGH_GRADIENT, 1, gray.rows / 8, 200, 50, 0, 0);
-
-    for (auto &vec : circles) {
-        Point center{cvRound(vec[0]), cvRound(vec[1])};
-        int radius = cvRound(vec[2]);
-        circle(image, center, 3, Scalar(0, 255, 0), -1);
-        circle(image, center, radius, Scalar(0, 0, 255), 3);
-    }
-
-    namedWindow("example");
-    imshow("example", image);
-
-    cout << circles.size() << endl;
-    for (auto &vec : circles) {
-        cout << cvRound(vec[2]);
-        cout << vec << "| ";
-    }
-    cout << endl;
-
-    waitKey();
-
-/*
- * 250, 50 works for modeled images
- * 120, 50 works with observed images
- */
-
-    return;
-}
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -74,17 +18,26 @@ int main(int argc, char **argv) {
             cout << "invalid data path: " << data_path << endl;
             return EXIT_FAILURE;
         }
-    } catch (filesystem_error& error) {
+    } catch (filesystem_error &error) {
         cerr << error.what() << endl;
         return EXIT_FAILURE;
     }
 
     directory_iterator begin{data_path}, end;
     vector<directory_entry> entries{begin, end};
+    random_shuffle(entries.begin(), entries.end()); // randomize to eliminate bias
 
     Prompter prompter;
     for (auto &entry : entries) {
-        cout << entry.path() << endl;
         prompter.prompt(entry.path());
+    }
+
+    auto &table = prompter.get_table();
+    for (auto &entry : table) {
+        cout << entry.first << ": ";
+        for (double angle : entry.second) {
+            cout << angle << ", ";
+        }
+        cout << endl;
     }
 }
