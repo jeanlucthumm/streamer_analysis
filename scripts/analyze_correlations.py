2116eb712b279d56bb1b589543a0d3b9b589513c
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2017-07-19 13:46:14 -0700

    Compute average

--- scripts/analyze_correlations.py
+++ scripts/analyze_correlations.py
@@ -1,5 +1,28 @@
 import csv
 import sys
+import utils
+
+
+def average(reader):
+    total = 0
+    count = 0
+    for row in reader:
+        if len(row) == 0:
+            print('malformed row', row)
+            continue
+
+        for delta in row[1:]:
+            try:
+                total += float(delta)
+                count += 1
+            except ValueError:
+                print('could not process delta:', delta, 'for prefix:', row[0])
+
+    if count == 0:
+        print('no delta values found in input file')
+        return
+
+    return round(total / float(count), utils.ROUND_ERR)
 
 
 def main(argv):
@@ -10,11 +33,8 @@ def main(argv):
         print('Reading file', file.name, '...')
         file.readline()  # ignore header
         reader = csv.reader(file, delimiter=',')
-        for row in reader:
-            if len(row) == 0:
-                print('malformed row:', row)
-                continue
-            print(row)
+        print('Average delta:', average(reader))
+        file.seek(0, 0)
 
 
 if __name__ == '__main__':
