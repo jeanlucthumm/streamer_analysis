5dd1996bbe9ceb8c8085f3a8ecc40fcf57ce6f2c
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2017-07-19 11:51:30 -0700

    get_name_data can now handle file extensions

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,11 @@
 def get_name_data(name: str):
-    parts = name.split('-')
+    """
+    Split and validate a name string into relevant information
+    :param name: name of the image
+    :return: (prefix, 'o'/'s') or None if name is invalid
+    """
+    base_name = name.split('.')[0]
+    parts = base_name.split('-')
     if len(parts) != 2 or parts[1] not in ['s', 'o']:
         return None
     else:
