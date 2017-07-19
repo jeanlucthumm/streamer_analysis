243d03b36eb01b017c47180e34f772cd2fd1d618
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2017-07-18 17:50:59 -0700

    Add get_name_data to utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,6 @@
 def get_name_data(name: str):
     parts = name.split('-')
-    
-
+    if len(parts) != 2 or parts[1] not in ['s', 'o']:
+        return None
+    else:
+        return parts[0], parts[1]
