5baf8f03c401093e0fe9c21a9aa658003bf2118d
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2017-07-26 15:12:24 -0700

    PairList skeleton

---
+++ src/PairList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jean-Luc Thumm on 7/26/17.
+//
+
+#include "PairList.h"
---
+++ src/PairList.h
@@ -0,0 +1,14 @@
+//
+// Created by Jean-Luc Thumm on 7/26/17.
+//
+
+#ifndef STEREO_READER_PAIRLIST_H
+#define STEREO_READER_PAIRLIST_H
+
+
+class PairList {
+
+};
+
+
+#endif //STEREO_READER_PAIRLIST_H
